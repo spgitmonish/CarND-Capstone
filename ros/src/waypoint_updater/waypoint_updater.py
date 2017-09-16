b3acefd4fb477c28e2975a9dda85b698596773bb
cygnus77 <ananddinakar@gmail.com>
2017-09-16 11:12:02 -0400

    caching wapoint distances

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 
 import math
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -21,12 +22,15 @@ as well as to verify your TL classifier.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+MAX_SPEED = 20 # m/s
 
+def cartesian_distance(a, b):
+    return math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
 
 class WaypointUpdater(object):
     def __init__(self):
-        rospy.init_node('waypoint_updater')
 
+        rospy.init_node('waypoint_updater')
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
@@ -39,13 +43,16 @@ class WaypointUpdater(object):
 
         self.current_pose = None
         self.base_lane = None
+        self.base_waypoints = None
+        self.num_waypoints = 0
+        self.base_waypoint_distances = None
 
         rospy.spin()
 
     # called when car's pose has changed
     # respond by emitting next set of final waypoints
     def pose_cb(self, msg):
-        if self.base_lane.waypoints == None:
+        if self.base_waypoints == None:
             return
 
         rospy.loginfo("pose_cb::x:%f,y:%f,z:%f; qx:%f,qy:%f,qz:%f,qw:%f", 
@@ -54,17 +61,35 @@ class WaypointUpdater(object):
         self.current_pose = msg
 
         # find nearest waypoint
-        self.updateNearestWaypointIndex(self.base_lane.waypoints, self.current_pose)
-        wp1 = self.nearestWaypointIndex
+        wp1 = self.getNearestWaypointIndex(self.current_pose)
+        self.nearestWaypointIndex = wp1
         rospy.loginfo("closest: %d", wp1)
 
+        """
+        # index of trajectory end
+        wp2 = (wp1 + LOOKAHEAD_WPS)%self.num_waypoints
+
+        # distance to end of trajectory
+        d = self.distance(waypoints, wp1, wp2)
+
+        # time to reach end
+        t = d / MAX_SPEED
+
+        p1 = self.base_waypoints[wp1].pose.pose.position
+        p2 = self.base_waypoints[wp1].pose.pose.position
+        jmt = JMT([], [], t)
+        """
+
         # return next n waypoints as a Lane pbject
-        waypoints = self.base_lane.waypoints[wp1:(wp1 + LOOKAHEAD_WPS)%len(self.base_lane.waypoints)]
+        waypoints = self.base_waypoints[wp1:(wp1 + LOOKAHEAD_WPS)%self.num_waypoints]
         lane = Lane()
         lane.header.frame_id = '/world'
         lane.header.stamp = rospy.Time(0)
         lane.waypoints = waypoints
         self.final_waypoints_pub.publish(lane)
+
+
+
 
         # TODO:
 
@@ -84,55 +109,84 @@ class WaypointUpdater(object):
 
         #7. Generate map coordinate points as fixed time intervals (t=0.2)
 
+    def JMT(start, end, T):
+        """
+        Calculate the Jerk Minimizing Trajectory that connects the initial state
+        to the final state in time T.
 
+        INPUTS
 
+        start - the vehicles start location given as a length three array corresponding to initial values of [s, s_dot, s_double_dot]
+        end   - the desired end state for vehicle. Like "start" this is a length three array.
+        T     - The duration, in seconds, over which this maneuver should occur.
+
+        OUTPUT: an array of length 6, each value corresponding to a coefficent in the polynomial 
+        s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+
+        EXAMPLE:
+        > JMT( [0, 10, 0], [10, 10, 0], 1)
+        [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+        """
+        t2 = T * T
+        t3 = t2 * T
+        t4 = t2 * t2
+        t5 = t3 * t2
+        Tmat = np.array( [[t3, t4, t5], [3*t2, 4*t3, 5*t4], [6*T, 12*t2, 20*t3]] )
+
+        Sf = end[0]
+        Sf_d = end[1]
+        Sf_dd = end[2]
+        Si = start[0]
+        Si_d = start[1]
+        Si_dd = start[2]
+
+        Sfmat = np.array( [[Sf - (Si + Si_d*T + 0.5*Si_dd*T*T)], [Sf_d - (Si_d + Si_dd*T)], [Sf_dd - Si_dd]] )
+        alpha = np.linalg.inv(Tmat).dot(Sfmat)
+        return (Si, Si_d, 0.5*Si_dd, alpha[0], alpha[1], alpha[2])
 
     # update nearest waypoint index by searching nearby values
     # waypoints are sorted, so search can be optimized
-    def updateNearestWaypointIndex(self, waypoints, pose):  
+    def getNearestWaypointIndex(self, pose):  
         # func to calculate cartesian distance
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
 
         # previous nearest point not known, do exhaustive search
         # todo: improve with binary search
         if self.nearestWaypointIndex == -1:    
-            r = [(dl(wp.pose.pose.position, pose.pose.position), i) for i,wp in enumerate(waypoints)]
-            self.nearestWaypointIndex = min(r, key=lambda x: x[0])[1]
-            return
+            r = [(dl(wp.pose.pose.position, pose.pose.position), i) for i,wp in enumerate(self.base_waypoints)]
+            return min(r, key=lambda x: x[0])[1]
 
         # previous nearest waypoint known, so scan points immediately after (& before)
         else:
-            numpoints = len(waypoints)
-            d = dl(waypoints[self.nearestWaypointIndex].pose.pose.position, pose.pose.position)
+            
+            d = dl(self.base_waypoints[self.nearestWaypointIndex].pose.pose.position, pose.pose.position)
             # scan right
             i = self.nearestWaypointIndex
             d1 = d
             found = False
             while True:
-                i = (i + 1) % numpoints
-                d2 = dl(waypoints[i].pose.pose.position, pose.pose.position)
+                i = (i + 1) % self.num_waypoints
+                d2 = dl(self.base_waypoints[i].pose.pose.position, pose.pose.position)
                 if d2 > d1: break
                 d1 = d2
                 found = True
             if found:
-                self.nearestWaypointIndex = i-1
-                return
+                return i-1
 
             # scan left
             i = self.nearestWaypointIndex
             d1 = d
             found = False
             while True:
-                i = (i - 1) % numpoints
-                d2 = dl(waypoints[i].pose.pose.position, pose.pose.position)
+                i = (i - 1) % self.num_waypoints
+                d2 = dl(self.base_waypoints[i].pose.pose.position, pose.pose.position)
                 if d2 > d1: break
                 d1 = d2
                 found = True
             if found:
-                self.nearestWaypointIndex = i+1
-                return
+                return i+1
 
-            return # keep prev value
+            return self.nearestWaypointIndex# keep prev value
 
 
     # Waypoint callback - data from /waypoint_loader
@@ -142,6 +196,18 @@ class WaypointUpdater(object):
             rospy.loginfo("waypoints_cb::%d", len(base_lane.waypoints))
             self.nearestWaypointIndex = -1
             self.base_lane = base_lane
+            self.base_waypoints = base_lane.waypoints
+            self.num_waypoints = len(self.base_waypoints)
+            self.base_waypoint_distances = []
+            d = 0.
+            pos1 = self.base_waypoints[0].pose.pose.position
+            for i in range(self.num_waypoints):
+                pos2 = self.base_waypoints[i].pose.pose.position
+                gap = cartesian_distance(pos1,pos2)
+                self.base_waypoint_distances.append(d + gap)
+                d += gap
+                pos1 = pos2
+            rospy.loginfo("track length: %f", d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
