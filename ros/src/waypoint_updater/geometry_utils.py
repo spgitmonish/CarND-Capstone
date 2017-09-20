be898e828e9115fa0d98dfb678902da5e5018cca
danbergeland <danbergeland@gmail.com>
2017-09-20 00:10:28 -0500

    adds geometry_utils file for helper functions

---
+++ ros/src/waypoint_updater/geometry_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from math import cos, sin
+
+def clip(val, min, max):
+    if val < min:
+        return min
+    if val > max:
+        return max
+    return val
+
+def cartesian_distance(a, b):
+    return math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+
+def theta_slope(w1, w2):
+    x1 = w1.pose.pose.position.x
+    y1 = w1.pose.pose.position.y
+    x2 = w2.pose.pose.position.x
+    y2 = w2.pose.pose.position.y
+    return math.atan2((y2-y1), (x2-x1))
+
+def XYGlobalToLocal(xglobal,yglobal,car_x,car_y,car_yaw):
+    shift_x = xglobal - car_x
+    shift_y = yglobal - car_y
+    x = shift_x*cos(0-car_yaw)-shift_y*sin(0-car_yaw)
+    y = shift_x*sin(0-car_yaw)+shift_y*cos(0-car_yaw)
+    return x, y
+
+def XYLocalToGlobal(xlocal, ylocal, car_x, car_y, car_yaw):
+    x = xlocal*cos(car_yaw)-ylocal*sin(car_yaw)+car_x
+    y = xlocal*sin(car_yaw)+ylocal*cos(car_yaw)+car_y
+    return x, y
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import rospy
-import tf
-from geometry_msgs.msg import Pose, Point, PoseStamped, TwistStamped, Quaternion
-from styx_msgs.msg import Lane, Waypoint
-
 import math
 import numpy as np
 from scipy.interpolate import interp1d, splrep, splev
 from eventlet import event
+import rospy
+import tf
+
+from geometry_msgs.msg import Pose, Point, PoseStamped, TwistStamped, Quaternion
+from styx_msgs.msg import Lane, Waypoint
+
+import geometry_utils
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -36,23 +38,6 @@ LIGHT_BREAKING_DISTANCE_METERS = 80 # meters
 FSM = {'STOP':0,
         'GO':1,
         'STOPPING':2}
-
-def clip(val, min, max):
-    if val < min:
-        return min
-    if val > max:
-        return max
-    return val
-
-def cartesian_distance(a, b):
-    return math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-
-def theta_slope(w1, w2):
-    x1 = w1.pose.pose.position.x
-    y1 = w1.pose.pose.position.y
-    x2 = w2.pose.pose.position.x
-    y2 = w2.pose.pose.position.y
-    return math.atan2((y2-y1), (x2-x1))
 
 class WaypointUpdater(object):
     def __init__(self):
